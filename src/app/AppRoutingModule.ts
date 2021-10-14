b095fb0be8afa549383caf448469524a5a8b3b98
monotera <nelson.a.mosquera@gmail.com>
2021-10-14 00:35:21 -0500

    changes color

--- src/app/AppRoutingModule.ts
+++ src/app/AppRoutingModule.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'navigation', component: NavigationComponent},
   {path: 'info', component: InfoComponent},
-  {path: 'crew/:id', component: CrewmembersComponent},
+  {path: 'crew', component: CrewmembersComponent},
   {path: 'account', component: AccountComponent},
   {path: 'comerce', component: ComerceComponent}
 ];
