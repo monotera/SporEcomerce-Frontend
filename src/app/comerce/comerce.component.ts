a83fca0f4734bba03ef043e79dc1458ac8aec402
monotera <nelson.a.mosquera@gmail.com>
2021-10-15 05:51:43 -0500

    Creates buy method

--- src/app/comerce/comerce.component.ts
+++ src/app/comerce/comerce.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { Crew } from '../model/crew';
 import { Spaceship } from '../model/spaceship';
 import { CrewService } from '../shared/crew.service';
+import { Productxplanet } from '../model/productxplanet';
 @Component({
   selector: 'app-comerce',
   templateUrl: './comerce.component.html',
@@ -21,14 +22,15 @@ export class ComerceComponent implements OnInit {
   isLoaded: boolean = false
 
   constructor(private starService: StarService, private route: ActivatedRoute, private _router: Router,private crewService: CrewService) { }
-
+  crew_id: string | null = null
+  star_id:string|null = null
   ngOnInit(): void {
     //TODO: camiar cuando ya se sepan identificar los usuarios
     //http://localhost:4200/comerce?star_id=11&crew_id=219
-    const firstParam: string | null = this.route.snapshot.queryParamMap.get('star_id');
-    const secondParam: string | null = this.route.snapshot.queryParamMap.get('crew_id');
-    this.crewService.findCrew(Number(secondParam)).subscribe(crew => this.credits = crew.credits,err =>this._router.navigateByUrl('/crew_not_found'))
-    this.starService.findCrew(Number(firstParam)).subscribe(star => {
+    this.star_id = this.route.snapshot.queryParamMap.get('star_id');
+    this.crew_id = this.route.snapshot.queryParamMap.get('crew_id');
+    this.crewService.findCrew(Number(this.crew_id)).subscribe(crew => this.credits = crew.credits,err =>this._router.navigateByUrl('/crew_not_found'))
+    this.starService.findCrew(Number(this.star_id)).subscribe(star => {
       this.star = star
       this.star.planetList.forEach(planet => {
         planet.product_list.forEach(pxp => {
@@ -46,9 +48,18 @@ export class ComerceComponent implements OnInit {
   trackByIndex(index: number, obj: any): any {
     return index;
   }
-  buy(input_index: number,pxp_id:number) {
+  buy(input_index: number,pxp:Productxplanet) {
     console.log(this.amounts[input_index])
-    console.log(pxp_id)
+    if (this.amounts[input_index] < 1 || this.amounts[input_index] > pxp.stock) {
+      alert("Error! illegal action!")
+      return
+    }
+    this.starService.buyProduct(this.amounts[input_index], pxp.id, Number(this.crew_id)).subscribe(res => {
+      alert("Your transaction was successful!")
+      window.location.reload(true)
+    }, err => {
+      alert("Error!!")
+    })
   }
   sell(input_index: number, pxp_id: number) {
     console.log(this.amounts[input_index])
--- src/app/shared/star.service.ts
+++ src/app/shared/star.service.ts
@@ -1,18 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Productxplanet } from '../model/productxplanet';
 import { Star } from '../model/star';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarService {
-
+ httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 constructor(private http: HttpClient) {
     
   }
   findCrew(id: number):Observable<Star> {
     return this.http.get<Star>("http://localhost:8080/star?id=" + id)
     
+  }
+  buyProduct(amount: number, pxp_id: number, crew_id: number){
+    return this.http.put(`http://localhost:8080/pxp/buy?amountProducts=${amount}&pxp_id=${pxp_id}&crew_id=${crew_id}`,this.httpOptions);
   }
 }
