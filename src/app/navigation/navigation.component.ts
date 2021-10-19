06993498d531deb1136770c91bd8a5e64c46f098
CarlosAndresErazoGarzon <carlos-erazo@javeriana.edu.co>
2021-10-19 02:48:28 -0500

    performs some test

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -17,9 +17,9 @@ export class NavigationComponent implements OnInit, OnDestroy {
 
   star: Star = new Star(-1,-1,-1,-1,"",null,false,null, null)
   player: Player = new Player(-1,"","",new Crew(-1,"",0,0,null,new Spaceship(0,"",0,0)))
-  starList: Star[] = [new Star(-1,-1,-1,-1,"Amarilla",null,false, null, null),
-                      new Star(-1,-1,-1,-1,"Azul",null,false, null, null),
-                      new Star(-1,-1,-1,-1,"Azul",null,false, null, null),
+  starList: Star[] = [new Star(193,-1,-1,-1,"Amarilla",null,false, null, null),
+                      new Star(196,-1,-1,-1,"Azul",null,false, null, null),
+                      new Star(201,-1,-1,-1,"Azul",null,false, null, null),
                       new Star(-1,-1,-1,-1,"Azul",null,false, null, null)]
 
   constructor(private starService: StarService,private route: ActivatedRoute, private playerService: PlayerService) { }
